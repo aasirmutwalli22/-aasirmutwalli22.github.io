2d33d65b6bc773ce2cc2864d5e062719d25288b7
aasirmutwalli22 <aasirmutwalli22@gmail.com>
2023-07-08 20:15:16 +0530

    bootstrap and related packages are installed

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,23 @@
-import { resolve } from 'path'
+import path, { resolve } from 'path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-    root: 'src',
+    root: path.resolve(__dirname, 'src'),
+    publicDir: path.resolve(__dirname, 'src/public'),
+    server: {
+        port: 8080,
+        hot: true
+    },
     build: {
         rollupOptions: {
             input: {
                 main: resolve(__dirname, 'index.html')
             },
         },
+    },
+    resolve: {
+        alias: {
+            '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+        }
     },
 })
